1e7b2e655a08f074afc18567cbb8ac55c6ce1d78
jaxon <booga956@gmail.com>
2022-08-10 17:34:05 -0600

    Added button and label for Shop Menu

--- code/ui/shopMenu.cs
+++ code/ui/shopMenu.cs
@@ -10,15 +10,18 @@ public class shopMenu : Panel
     public shopMenu()
     {
         StyleSheet.Load("/ui/shopMenu.scss");
-        panel = Add.Panel();
-        upgradeMoney = panel.Add.Button("upgrade");
-        upgradeMoney.Text = "Upgrade Money";
-        upgradeMoney.Style.FontColor = Color.White;
+        panel = Add.Panel("shopMenuOP");
+        upgradeMoney = panel.Add.Button("Upgrade Money Per Click", "upgrade");
+        costUpgrade = panel.Add.Label("$0", "cost");
+        currentAmount = panel.Add.Label("$1", "current");
     }
 	public override void Tick()
 	{
 		base.Tick();
+        var ply = Local.Pawn as Pawn;
+        if(ply == null) return;
 
+        currentAmount.Text = $"$ssss{ply.moneyPerClick}";
         Parent.SetClass("shopMenuOpen", Input.Down(InputButton.Flashlight));
 	}
 }
