385020774fa90a8724ae8e9764b0d2157fb86235
jaxon <booga956@gmail.com>
2022-08-10 17:24:51 -0600

    Made Shopmenu look better, and added extra elements that you cant see

--- code/ui/shopMenu.cs
+++ code/ui/shopMenu.cs
@@ -4,10 +4,16 @@ using Sandbox.UI.Construct;
 public class shopMenu : Panel
 {
     Panel panel;
+    Button upgradeMoney;
+    Label costUpgrade;
+    Label currentAmount;
     public shopMenu()
     {
         StyleSheet.Load("/ui/shopMenu.scss");
         panel = Add.Panel();
+        upgradeMoney = panel.Add.Button("upgrade");
+        upgradeMoney.Text = "Upgrade Money";
+        upgradeMoney.Style.FontColor = Color.White;
     }
 	public override void Tick()
 	{
